5c9a999ac033a106a4f630cca1c7aeeaebeaa394
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-13 11:53:56 -0300

    Veiculos add metodo

--- veiculos/src/br/com/fiap/Caminhao.java
+++ veiculos/src/br/com/fiap/Caminhao.java
@@ -1,12 +1,36 @@
 package br.com.fiap;
 
-import java.util.Date;
+import java.text.ParseException;
 
-public  final class Caminhao extends Veiculo {
+public final class Caminhao extends Veiculo {
 
-	public Caminhao(String marca, Date anofabricacao, String cor, int velocidade, Boolean ligado) {
-		super(marca, anofabricacao, cor, velocidade, ligado);
-		// TODO Auto-generated constructor stub
+	private int rodas;
+
+	public Caminhao(String nomeModelo, String marca, String cor, String embreagem, String tipoCombustivel,
+			String anoFabricacao, int velocidade, int maxVelocidade, double combustivel, double peso,
+			boolean estaLigado, boolean estaOnfarol, int rodas) throws ParseException {
+		super(nomeModelo, marca, cor, embreagem, tipoCombustivel, anoFabricacao, velocidade, maxVelocidade, combustivel,
+				peso, estaLigado, estaOnfarol);
+		this.rodas = rodas;
+	}
+
+	@Override
+	public void ShowVeiculosStatus() {
+		System.out.println("Modelo:" + this.nomeModelo);
+		System.out.println("Marca:" + this.marca);
+		System.out.println("Cor:" + this.cor);
+		System.out.println("Embreagem:" + this.embreagem);
+		System.out.println("Ano de fabriaçao:" + this.anoFabricacao);
+		System.out.println("tipo Combustivel:" + this.tipoCombustivel);
+		System.out.println();
+	}
+
+	@Override
+	public void Buzina() {
+		if(this.estaLigado){
+			System.out.println("Buz Buz");
+			}
+		
 	}
 
 }
